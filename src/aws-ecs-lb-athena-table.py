2cffe0f5de9e407f7d09a6d8a56eee3ff9d8ebfa
Dave North <dnorth@signiant.com>
2017-10-16 15:05:20 -0400

    break things up as we need quite a bit more Athena stuff

--- src/aws-ecs-lb-athena-table.py
+++ src/aws-ecs-lb-athena-table.py
@@ -1,131 +1,8 @@
 import argparse
 import boto3
 import sys
-
-
-# Checks whether logging is enabled
-# For an ALB, load_balancer is an ARN; it's a name for an ELB
-def check_elb_logging_status(load_balancer, load_balancer_type, boto_session):
-    logging_enabled = False
-
-    if load_balancer_type == 'ALB':
-        alb_client = boto_session.client("elbv2")
-
-        try:
-            response = alb_client.describe_load_balancer_attributes(
-                LoadBalancerArn=load_balancer
-            )
-        except Exception as e:
-            print("Error obtaining attributes for ALB " + load_balancer + " (" + str(e) + ")")
-
-        if response:  # ALB attributes are a key/value list and return a STRING
-            for attrib in response['Attributes']:
-                if attrib['Key'] == 'access_logs.s3.enabled' and attrib['Value'] == 'true':
-                    logging_enabled = True
-    elif load_balancer_type == 'ELB':
-        elb_client = boto_session.client('elb')
-
-        try:
-            response = elb_client.describe_load_balancer_attributes(
-                LoadBalancerName=load_balancer
-            )
-        except Exception as e:
-            print("Error obtaining attributes for ELB " + load_balancer + " (" + str(e) + ")")
-
-        if response:  # ELB attributes are a set object and return a BOOLEAN
-            logging_enabled = response['LoadBalancerAttributes']['AccessLog']['Enabled']
-
-    return logging_enabled
-
-
-# Enable access logging on an ALB/ELB
-# For an ALB, load_balancer is an ARN; it's a name for an ELB
-def enable_elb_access_logging(load_balancer, load_balancer_type, bucket, bucket_prefix, boto_session):
-    status = False
-
-    print("enabling access logs for " + load_balancer + " writing to bucket " + bucket + " prefix " + bucket_prefix)
-
-    if load_balancer_type == 'ALB':
-        elb_client = boto_session.client('elbv2')
-
-        try:
-            response = elb_client.modify_load_balancer_attributes(
-                LoadBalancerArn=load_balancer,
-                Attributes=[
-                    {
-                        'Key': 'access_logs.s3.enabled',
-                        'Value': 'true'
-                    },
-                    {
-                        'Key': 'access_logs.s3.bucket',
-                        'Value': bucket
-                    },
-                    {
-                        'Key': 'access_logs.s3.prefix',
-                        'Value': bucket_prefix
-                    }
-                ]
-            )
-
-            # We don't care about the response and it will except if there's any issue
-            status = True
-        except Exception as e:
-            print("Error setting attributes for ALB " + load_balancer + " (" + str(e) + ")")
-            status = False
-    elif load_balancer_type == 'ELB':
-        elb_client = boto_session.client('elb')
-
-        try:
-            response = elb_client.modify_load_balancer_attributes(
-                LoadBalancerAttributes={
-                    'AccessLog': {
-                        'Enabled': True,
-                        'S3BucketName': bucket,
-                        'EmitInterval': 5,
-                        'S3BucketPrefix': bucket_prefix
-                    },
-                },
-                LoadBalancerName=load_balancer,
-            )
-
-            if response:
-                if response['ResponseMetadata']['HTTPStatusCode'] == 200:
-                    status = True
-        except Exception as e:
-            print("Error setting attributes for ELB " + load_balancer + " (" + str(e) + ")")
-            status = False
-
-    return status
-
-
-def create_athena_table(force, bucket, service_name, session):
-    print "create athena table"
-
-
-def lookup_alb_arn(target_group_arn, boto_session):
-    print("Looking up LB arn for target group " + target_group_arn)
-    alb_client = boto_session.client("elbv2")
-
-    alb_arn = None
-
-    # This whole process kinda violates the schema since a target group can be associated with
-    # multiple ALBs.  However, in our case that's not the case
-    try:
-        response = alb_client.describe_target_groups(
-            TargetGroupArns=[
-                target_group_arn
-            ],
-        )
-    except Exception as e:
-        print("Error obtaining info for target group " + target_group_arn + " (" + str(e) + ")")
-
-    if response:
-        # get ARN of the first load balancer for this target group
-        alb_arn = response['TargetGroups'][0]['LoadBalancerArns'][0]
-
-        print("ALB arn determined to be " + alb_arn)
-
-    return alb_arn
+import aws_elb  # Module in this project
+import aws_athena  # Module in this project
 
 
 def main(argv):
@@ -168,7 +45,7 @@ def main(argv):
                         # ok, we have a load balancer - what type is it?
                         if 'targetGroupArn' in loadbalancer_info:
                             print("Found an ALB for service " + service_name)
-                            load_balancer = lookup_alb_arn(loadbalancer_info['targetGroupArn'], session)
+                            load_balancer = aws_elb.lookup_alb_arn(loadbalancer_info['targetGroupArn'], session)
                             load_balancer_type = "ALB"
                         elif 'loadBalancerName' in loadbalancer_info:
                             print("Found an ELB Classic for service " + service_name)
@@ -182,9 +59,9 @@ def main(argv):
                         if load_balancer:
                             # Check and see if logging is enabled.  If not, enable it
                             state = False
-                            if not check_elb_logging_status(load_balancer, load_balancer_type, session):
+                            if not aws_elb.check_elb_logging_status(load_balancer, load_balancer_type, session):
                                 print("Access logging is NOT enabled on " + load_balancer)
-                                state = enable_elb_access_logging(
+                                state = aws_elb.enable_elb_access_logging(
                                     load_balancer,
                                     load_balancer_type,
                                     args.bucket,
@@ -200,7 +77,7 @@ def main(argv):
                                 print("Access logging is already enabled on " + load_balancer)
 
                             if need_to_create_athena_table or args.force:
-                                create_athena_table(args.force, args.bucket, service_name, session)
+                                aws_athena.create_athena_table(args.force, args.bucket, service_name, session)
                     else:
                         print("No load balancer for " + service_name)
     except Exception as e:
---
+++ src/aws_athena.py
@@ -0,0 +1,13 @@
+# Functions for working with Athena
+
+
+def create_athena_table(force, bucket, service_name, session):
+    print "create athena table"
+
+
+def create_athena_database(name, session):
+    print "create_athena_database"
+
+    # client = session.client('athena')
+
+    # create database if not exists djn
